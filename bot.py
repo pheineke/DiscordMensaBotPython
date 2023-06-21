9ad136ea0cee30078e2ecf61c286bdfe73aee0a3
pheineke <p_heineke22@cs.uni-kl.de>
2023-06-22 01:58:30 +0200

    Added token reference

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 # bot.py
 import os
-
+import token
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv(token.token)
 
 client = discord.Client()
 
@@ -13,4 +13,4 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-client.run(TOKEN)
+client.run(token.txt)
