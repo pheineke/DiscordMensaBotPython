10459da54bf99b7c370f5e476920abddfbd97ecc
pheineke <p_heineke22@cs.uni-kl.de>
2023-06-22 22:55:52 +0200

    Added simple guild terminal list feature.

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 intents = discord.Intents.default()
 
@@ -18,8 +19,15 @@ client = commands.Bot(command_prefix = "+",intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
 
-
+    print(f'{client.user} is connected to the following guild:\n'
+          f'{guild.name}(id: {guild.id})\n'
+          )
+    
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n  {members}')
 
 client.run(TOKEN)
