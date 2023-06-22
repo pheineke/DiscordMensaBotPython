ae4748d3e86e6170b64ecded16221ff9a9d394fe
pheineke <p_heineke22@cs.uni-kl.de>
2023-06-22 02:16:13 +0200

    Bot py changed

--- bot.py
+++ bot.py
@@ -2,16 +2,24 @@
 import os
 
 import discord
+from discord.ext import commands
+
 from dotenv import load_dotenv
+
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+
+
+client = commands.Bot(command_prefix = "+",intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+
 
 client.run(TOKEN)
