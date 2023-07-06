57cd906c71992e860f5b6dc1638cfa35837434ae
pheineke <p_heineke22@cs.uni-kl.de>
2023-07-06 02:28:24 +0200

    readall users noch nicht funktionstüchtig. Wie lese ich aus einer JSON alle keys und values aus und gebe sie zurück

--- bot.py
+++ bot.py
@@ -1,20 +1,13 @@
 # bot.py
-import os
 
+import os
 import discord
 from discord.ext import commands
-
 from dotenv import load_dotenv
+
 #IMPORTS Discord..............END
-
-#import usertime
-import re
-import json
-
-
+import usertime
 #IMPORTS......................END
-
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -24,10 +17,9 @@ intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
+bot = commands.Bot(command_prefix = "",intents=intents)
 
-bot = commands.Bot(command_prefix = "+",intents=intents)
-
-
+#BOTEVENTS
 
 @bot.event
 async def on_ready():
@@ -39,32 +31,64 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
+    messagecontent = message.content.lower()
     print("Message Content: " + message.content)
 
-    neuevariable = message.content.split()
 
+    if 'my.' in messagecontent:
 
+        author = message.author.mention
+        print(author)
 
-    if 'My.' in message.content:
-        if 'Mensatime' in message.content:
-            await message.channel.send(neuevariable)
-            await message.channel.send("And the oscar goes to " + str(message.author.mention))
+        if 'mensatime' in messagecontent:
+            if ".del" in messagecontent:
+                try:
+                    usertime.userdelete(author)
+                except:
+                    await message.channel.send("Dieser User ist nicht vorhanden.")
+                else:
+                    await message.channel.send("Dein Eintrag wurde gelöscht.")
+
+            elif '=' in messagecontent:
+                try:
+                    uhrzeitsplitstring = messagecontent.split()[2].split(':')
+                except:
+                    await message.channel.send("Das ist keine gültige Uhrzeit.\nUhrzeiten werden folgendermaßen angegeben: HH:MM")
+
+                else:
+                    #await message.channel.send(uhrzeitsplitstring)
+
+                    try:
+                        uhrzeitsplitintcheck = [int(uhrzeitsplitstring[0])] + [int(uhrzeitsplitstring[1])]
+                    except:
+                        await message.channel.send("Das ist keine gültige Uhrzeit.\nUhrzeiten werden folgendermaßen angegeben: HH:MM")
+
+                    else:
+                        if (0 <= uhrzeitsplitintcheck[0] <= 24) and (0 <= uhrzeitsplitintcheck[1] <= 59):
+                                timestringmil = int("".join(uhrzeitsplitstring))
+
+                                #await message.channel.send(uhrzeitsplitstring)
+                                #await message.channel.send(timestringmil) 
+                                usertime.userwrite(author, timestringmil)
+
+                                await message.channel.send(str(author) + " deine Mensatime wurde auf " + usertime.userread(author) + "gestellt.")
+
+                        else: await message.channel.send("Das ist keine gültige Uhrzeit.\nUhrzeiten werden folgendermaßen angegeben: HH:MM")
+            elif "deinemom" or "mom" in messagecontent:
+                await message.channel.send("Deine Mutter passt in keine Zeitrechnung im Universum.")
+            else:
+                try:
+                    userread = usertime.userread(author)
+                except:
+                    await message.channel.send("Der User hat keinen Eintrag.")
+                else:
+                    await message.channel.send("Deine Uhrzeit beträgt " + str(userread))
+            
         else:
-            await message.channel.send('duhs ist Standort ' + str(message.author.mention))        
+            await message.channel.send("Du hast leider keinen gültigen Befehl eingegeben." + str(message.author.mention))
+    elif "xs.mensatime" in messagecontent: 
+        await message.channel.send("Mensazeiten für eingetragene User")
+        usertime.userreadall()
 
-
-        
-        #await message.channel.send('du hs')
-
-
-
-'''
-@bot.command()
-async def members(ctx):
-    for guild in client.guilds:
-        for member in guild.members:
-            print(member)
-
-'''
-
+#BOT >>RUN
 bot.run(TOKEN)
--- usertime.py
+++ usertime.py
@@ -1,17 +1,56 @@
 #usertime.py
 import json
+import time
 
 with open('usercache.json', 'r') as f:
         data = json.load(f)
+        
+
+def savefile():
+        with open('usercache.json', 'w') as f:
+                json.dump(data,f)
 
 def userread(user):
-        return data[user]
+        user = user.lower()
+        userdata = str(data[user])
+        userdatalen = len(userdata)
+        hour, minute = userdata[:userdatalen//2], userdata[userdatalen//2:]
+        if data[user] == "0":
+                minute = "00"
+        return (hour+minute)
+
+def userreadall():
+        keys = data.keys()
+        for key, value in data.items():
+            return str(key) + " " + str(value)
+        print(keys)
+        print(data.keys())
+
 
 def userwrite(user, time):
         user = user.lower()
         data[user] = time
-        with open('usercache.json', 'w') as f:
-                json.dump(data,f)
 
-userwrite("Joshie", 1340)
-userread("Joshie")
+        savefile()
+
+def userdelete(user):
+        user = user.lower()
+
+        if user in data:
+                del data[user]
+        else:
+                raise Exception("user does not exist")    
+
+        savefile()
+
+# TESTING:
+
+if __name__ == "__main__":
+        userwrite("Joshie", 1340)
+        time.sleep(5)
+        userwrite("Moritz", 1350)
+        time.sleep(5)
+        print(userread("Joshie"))
+        time.sleep(5)
+        userdelete("Joshie")
+        userreadall()
